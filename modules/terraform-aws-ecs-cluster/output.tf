5a210bc04bcd38902d13e4e6377f3c6afdebc0a9
suranga-jayalath <suranga@shoutoutlabs.io>
2024-08-10 13:48:37 +0530

    tf module

--- modules/terraform-aws-ecs-cluster/output.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_id" {
-  value       = aws_ecs_cluster.ecs_cluster.id
-  description = "AWS ECS Cluster ID"
-}
-
-output "cluster_name" {
-  value       = aws_ecs_cluster.ecs_cluster.name
-  description = "AWS ECS Cluster Name"
-}
